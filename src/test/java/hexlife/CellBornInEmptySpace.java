25bd7c846f01a896e8ec365e74046ee6472aba5e
Aki Salmi <aki.salmi@iki.fi>
2017-11-23 11:36:39 +0100

    Cell is not born, if too few first tier neighbours.

---
+++ src/test/java/hexlife/CellBornInEmptySpace.java
@@ -0,0 +1,4 @@
+package hexlife;
+
+public interface CellBornInEmptySpace {
+}
--- src/test/java/hexlife/RulesShould.java
+++ src/test/java/hexlife/RulesShould.java
@@ -13,32 +13,70 @@ public class RulesShould {
 
         FirstTierNeighbours firstTierNeighbours = new FirstTierNeighbours(2);
         SecondTierNeighbours secondTierNeighbours = new SecondTierNeighbours(1);
-        CellIsBornInEmptySpace isAlive = rules.isBornInEmptySpace(firstTierNeighbours, secondTierNeighbours);
+        CellBornInEmptySpace isAlive = rules.isBornInEmptySpace(firstTierNeighbours, secondTierNeighbours);
 
         assertThat(isAlive, equalTo(new CellIsBornInEmptySpace()));
     }
 
+    @Test
+    public void a_cell_is_not_born_if_too_few_first_tier_neighbours() {
+        Rules rules = new Rules();
+
+        FirstTierNeighbours firstTierNeighbours = new FirstTierNeighbours(0);
+        SecondTierNeighbours secondTierNeighbours = new SecondTierNeighbours(6);
+        CellBornInEmptySpace isAlive = rules.isBornInEmptySpace(firstTierNeighbours, secondTierNeighbours);
+
+        assertThat(isAlive, equalTo(new CellIsNotBornInEmptySpace()));
+    }
+
     private class Rules {
-        public CellIsBornInEmptySpace isBornInEmptySpace(FirstTierNeighbours firstTierNeighbours, SecondTierNeighbours secondTierNeighbours) {
-            return new CellIsBornInEmptySpace();
+        public CellBornInEmptySpace isBornInEmptySpace(FirstTierNeighbours firstTierNeighbours, SecondTierNeighbours secondTierNeighbours) {
+            if (firstTierNeighbours.weight() + secondTierNeighbours.weight() >= 2.3) {
+                return new CellIsBornInEmptySpace();
+            }
+            return new CellIsNotBornInEmptySpace();
         }
     }
 
-    private class CellIsBornInEmptySpace {
-
-        @Override
-        public boolean equals(Object obj) {
-            return true; // TODO see later what it is
-        }
-    }
 
     private class FirstTierNeighbours {
+
+        private final int numberOfNeighbours;
+
         public FirstTierNeighbours(int numberOfNeighbours) {
+            this.numberOfNeighbours = numberOfNeighbours;
+        }
+
+        public double weight() {
+            return numberOfNeighbours * 1.0;
         }
     }
 
     private class SecondTierNeighbours {
+
+        private final int numberOfNeighbours;
+
         public SecondTierNeighbours(int numberOfNeighbours) {
+            this.numberOfNeighbours = numberOfNeighbours;
+        }
+
+        public double weight() {
+            return numberOfNeighbours * 0.3;
         }
     }
+
+    private class CellIsBornInEmptySpace implements CellBornInEmptySpace {
+        @Override
+        public boolean equals(Object obj) {
+            return obj != null && getClass() == obj.getClass();
+        }
+    }
+
+    private class CellIsNotBornInEmptySpace implements CellBornInEmptySpace {
+        @Override
+        public boolean equals(Object obj) {
+            return obj != null && getClass() == obj.getClass();
+        }
+    }
+
 }
