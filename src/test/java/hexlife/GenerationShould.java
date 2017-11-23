cca4a66d3479f633aac729465f3b2e8e431ecbd9
Aki Salmi <aki.salmi@iki.fi>
2017-11-23 17:44:12 +0100

    added another test

--- src/test/java/hexlife/GenerationShould.java
+++ src/test/java/hexlife/GenerationShould.java
@@ -8,7 +8,7 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class GenerationShould {
 
     @Test
-    public void generationThatStaysTheSame() {
+    public void evolve_on_a_tick_and_creates_new_cells() {
         LivingCells seed = LivingCells.of(
                 new Cell('c', 3),
                 new Cell('c', 2),
@@ -28,6 +28,18 @@ public class GenerationShould {
                 new Cell('b', 3),
                 new Cell('d', 3)
         );
+        Generation expected = Generation.withSeed(nextGenerationCells);
+        assertThat(nextGeneration, equalTo(expected));
+    }
+
+    @Test
+    public void generation_should_remove_single_cell() {
+        LivingCells seed = LivingCells.of(new Cell('c', 2));
+        Generation generation = Generation.withSeed(seed);
+
+        Generation nextGeneration = generation.tick();
+
+        LivingCells nextGenerationCells = LivingCells.of();
         Generation expected = Generation.withSeed(nextGenerationCells);
         assertThat(nextGeneration, equalTo(expected));
     }
