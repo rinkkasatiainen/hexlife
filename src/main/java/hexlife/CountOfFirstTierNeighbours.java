fdca1bdb4eb682eb6870dbac3f9bb29fefcb40a4
Peter Kofler <peter.kofler@code-cop.org>
2017-12-06 23:30:56 +0100

    Remove equals/hashCode only for tests - not needed any more.

--- src/main/java/hexlife/CountOfFirstTierNeighbours.java
+++ src/main/java/hexlife/CountOfFirstTierNeighbours.java
@@ -12,26 +12,4 @@ class CountOfFirstTierNeighbours {
         return numberOfNeighbours * 1.0;
     }
 
-
-    // only for test, is this good? now we have the weight and could delete this code again
-    // on the other hand a value object by definition would need that because of "value"
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        CountOfFirstTierNeighbours that = (CountOfFirstTierNeighbours) o;
-        return numberOfNeighbours == that.numberOfNeighbours;
-    }
-
-    @Override
-    public int hashCode() {
-        return numberOfNeighbours;
-    }
-
-    @Override
-    public String toString() {
-        return "CountOfFirstTierNeighbours{" + numberOfNeighbours + '}';
-    }
 }
